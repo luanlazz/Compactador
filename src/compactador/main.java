47d8a359ca3b668c8a4a72ee2e5b55f733ed3670
Henrique Machiavelli <hsmachiavelli@gmail.com>
2018-09-19 22:18:21 -0300

    ajustes na pipeline

--- src/compactador/main.java
+++ src/compactador/main.java
@@ -1,27 +1,35 @@
 package compactador;
 
-import FileHandler.FileHandler;
-
 import java.io.IOException;
+
+import FileHandler.FileHandler;
+import RLE.RLE;
+import BWT.BWT;
+import Huffman.*;
 
 public class main {
 
 	public static void main(String[] args) {
-		
 		String file = "";
 
 		final String path = "alice29.txt";
 
 		try {
 			file = new FileHandler().read(path);
+			
+			/*
+			 * PIPELINE:
+			 * BWT -> RLE -> Huffman
+			 */
+
+			String BWTEncoded = BWT.encode(file);
+			String RLEEncoded = RLE.encode(BWTEncoded);
+			
+			System.out.println(RLEEncoded);
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		
-		
-
 	}
-
 }
